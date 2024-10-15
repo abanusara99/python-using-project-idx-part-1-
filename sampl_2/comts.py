f02a615f4391325b674a54ca980be230bd038e31
Anupriya <96963618+abanusara99@users.noreply.github.com>
2024-10-15 08:11:45 +0530

    Update comts.py

--- sampl_2/comts.py
+++ sampl_2/comts.py
@@ -1,1 +1,16 @@
+# This is a single-line comment , Single line comment used to ignore certain code in the program. Create Document code to under what is the code used for
+print("Hello, World!")  # Output : Hello , World
+
+# This is a multi-line comment
+# that spans several lines.
+print("Hello, World!")  # Output : Hello , World
+
+
+"""
+This is another way to write
+a multi-line comment in Python
+named as Triple Quotes
+"""
+print("Hello, World!")
+
 
